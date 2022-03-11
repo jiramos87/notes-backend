7ec526d13f744da7a9a62002797b88b5bd9e9ac7
jiramos87 <jiramos87@gmail.com>
2022-03-11 16:51:12 -0300

    create note added

--- index.js
+++ index.js
@@ -42,13 +42,9 @@ app.get('/api/notes', (request, response) => {
 
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 const generateId = () => {
@@ -59,24 +55,21 @@ const generateId = () => {
 }
   
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
-    }
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
