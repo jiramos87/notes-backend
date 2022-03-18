3f8d4f13943bba68a63fc640bfaa6aef7227c647
jiramos87 <jiramos87@gmail.com>
2022-03-18 12:36:50 -0300

    uibuild

--- index.js
+++ index.js
@@ -72,11 +72,6 @@ app.get('/api/notes/:id', (request, response, next) => {
 app.put('/api/notes/:id', (request, response, next) => {
   const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
   Note.findByIdAndUpdate(
       request.params.id,
       { content, important },
